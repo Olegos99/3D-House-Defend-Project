83f5726c894c4b39138764c274047fe02231616d
Oleg <oleg.magarill@gmail.com>
2020-07-28 16:22:51 +0300

    camera work

--- 3D House Defend/Assets/Script/Player/Movement/v1/MouseLook.cs
+++ 3D House Defend/Assets/Script/Player/Movement/v1/MouseLook.cs
@@ -10,6 +10,8 @@ public class MouseLook : MonoBehaviour
     public Transform PlayerBody, Target;
     MovementControl movementControl;
 
+    public float CameraDistancecontroller;
+
     float xRotation = 0f;
     float yRotation = 0f;
 
@@ -18,58 +20,157 @@ public class MouseLook : MonoBehaviour
 
     float m_MouseX, m_MouseY;
 
+    private float CameraMaxPitch = 45f;
+    private float CameraMinPitch = -45f;
+
     public bool EnableMouseRotation = true;
     public bool CameraFreeezeCoorutineIsStarted = false;
+
+    float CurrentDictanceBetwenCameraAndTarget;
+
+    Vector3 StartingPosition;
+    Vector3 CurrentPosition;
+
+    private Vector3 DirectionFromCameraToTarget;
+
+    private float MaxDictanceBetwenCameraAndTarget;
+    private float MinDictanceBetwenCameraAndTarget = 2f;
+
+    float LastFrameMouseY;
+    public float DesiredDistanceFromTarget;
+
+    public Transform MovingCameraPosition;
+    public float smoothTime = 0.3F;
+    private Vector3 velocity = Vector3.zero;
+
     void Start()
     {
+        StartingPosition = this.gameObject.transform.position;
+        MaxDictanceBetwenCameraAndTarget = Vector3.Distance(Target.position, transform.position);
+        Debug.Log("MaxDictanceBetwenCameraAndTarget is:  " + MaxDictanceBetwenCameraAndTarget);
         Cursor.lockState = CursorLockMode.Locked; //lock cursor add keep it invisible(Esc to see it again)
         movementControl = GetComponentInParent<MovementControl>();
+
+        PlayerBody.rotation = Quaternion.RotateTowards(PlayerBody.rotation, Quaternion.Euler(0, m_MouseX - 180, 0), smoth * Time.deltaTime);
     }
 
-    // Update is called once per frame
-    void LateUpdate()
+
+    void Update()
     {
         CamControll();
     }
 
+
     void CamControll()
     {
-        //if (EnableMouseRotation)
-        //{
-         float MouseX = Input.GetAxis("Mouse X") * MouseSensitivity * Time.deltaTime;
-         float MouseY = Input.GetAxis("Mouse Y") * MouseSensitivity * Time.deltaTime;
-
-
         m_MouseX += Input.GetAxis("Mouse X") * MouseSensitivity;
         m_MouseY -= Input.GetAxis("Mouse Y") * MouseSensitivity;
-
-        m_MouseY = Mathf.Clamp(m_MouseY, -45f, 60f);
-        //MouseY = Mathf.Clamp(MouseY, -45f, 20f);
-
-        xRotation -= MouseY;
-        xRotation = Mathf.Clamp(xRotation, -45f, 20f);
-
-        //yRotation += MouseX;
-
-        transform.LookAt(Target);
+        m_MouseY = Mathf.Clamp(m_MouseY, CameraMinPitch, CameraMaxPitch);//camera rotation limitation
 
         Target.rotation = Quaternion.Euler(m_MouseY, m_MouseX, 0);
+        transform.LookAt(Target);
+
+        var Controller = Mathf.Clamp(m_MouseY, -100f, 100f);
+
+        if (Controller > 0)
+        {
+            DesiredDistanceFromTarget = MaxDictanceBetwenCameraAndTarget - Controller / 10;
+        }
+        else
+        {
+            DesiredDistanceFromTarget = MaxDictanceBetwenCameraAndTarget + Controller / 10;
+        }
+
+        DesiredDistanceFromTarget = Mathf.Clamp(DesiredDistanceFromTarget, 2f, 6f);
 
         if (EnableMouseRotation)
         {
-            //transform.localRotation = Quaternion.Euler(xRotation, 0f, 0f);
-
-            //Target.rotation = Quaternion.Euler(MouseY, MouseX, 0);
-
 
             if (movementControl.isMoving) // if make camera move around player while not moving this lines will apply player body rotation to veiw
             {
-                //PlayerBody.Rotate(Vector3.up * MouseX);
-                PlayerBody.rotation = Quaternion.RotateTowards(PlayerBody.rotation, Quaternion.Euler(0, m_MouseX, 0), smoth * Time.deltaTime);
+
+                //PlayerBody.Rotate(Vector3.up * MouseX);     
+                PlayerBody.rotation = Quaternion.RotateTowards(PlayerBody.rotation, Quaternion.Euler(0, m_MouseX - 180, 0), smoth * Time.deltaTime); //need correction (i use local rotations and it turned all player body 180)    
+
+                if (PlayerBody.rotation == Quaternion.Euler(0, m_MouseX - 180, 0))
+                {
+                    transform.position = Vector3.SmoothDamp(transform.position, MovingCameraPosition.position, ref velocity, smoothTime);
+                }
+
+                CameraMaxPitch = 10f;
+                CameraMinPitch = -10f;
+
                 //    PlayerBody.Rotate(Vector3.up * MouseX);
-                //    //PlayerBody.rotation = Quaternion.Euler(0, MouseX, 0);
+                //    PlayerBody.rotation = Quaternion.Euler(0, MouseX, 0);
+            }
+            else
+            {
+                CameraMaxPitch = 45f;
+                CameraMinPitch = -45f;
+
+                Vector3 NewCameraPosition = new Vector3(this.transform.localPosition.x, this.transform.localPosition.y, DesiredDistanceFromTarget);
+                
+                //this.transform.localPosition = Vector3.Lerp(this.transform.localPosition, NewCameraPosition, 1);
+                this.transform.localPosition = Vector3.SmoothDamp(transform.localPosition, NewCameraPosition, ref velocity, smoothTime);
             }
         }
+
+
+
+
+        //DirectionFromCameraToTarget = Target.position - transform.position;
+        //CurrentDictanceBetwenCameraAndTarget = Vector3.Distance(Target.position, transform.position);
+
+        
+       
+
+
+        //this.transform.position = Vector3.Lerp(this.transform.position, Target.position, (1 * Time.deltaTime));//make it first person))
+        //this.transform.rotation = Quaternion.Lerp(this.transform.rotation, Target.rotation, (1 * Time.deltaTime));
+
+
+        //transform.position = Vector3.Lerp(zoomstart.position, zoomend.position, zoom);
+
+
+        //this.transform.position = new Vector3(this.transform.position.x, this.transform.position.y, DesiredDictanseFromTarget);
+        //this.transform.position = DirectionFromCameraToTarget * DesiredDictanseFromTarget;
+
+        //if (CurrentDictanceBetwenCameraAndTarget < MaxDictanceBetwenCameraAndTarget && CurrentDictanceBetwenCameraAndTarget > MinDictanceBetwenCameraAndTarget)
+        //{
+        //    if (Mathf.Clamp(m_MouseY, -1f, 1f) > 0)
+        //    {
+        //        if (m_MouseY > LastFrameMouseY)
+        //        {
+        //            this.transform.position += DirectionFromCameraToTarget / 100 * 0.5f;
+        //        }
+        //        if (m_MouseY < LastFrameMouseY)
+        //        {
+        //            this.transform.position -= DirectionFromCameraToTarget / 100 * 0.5f;
+        //        }
+        //    }
+        //    else
+        //    {
+        //        if (m_MouseY > LastFrameMouseY)
+        //        {
+        //            this.transform.position += DirectionFromCameraToTarget / 100 * -0.5f;
+        //        }
+        //        if (m_MouseY < LastFrameMouseY)
+        //        {
+        //            this.transform.position -= DirectionFromCameraToTarget / 100 * -0.5f;
+        //        }
+        //    }
+
+        //}
+        //else if (CurrentDictanceBetwenCameraAndTarget > MaxDictanceBetwenCameraAndTarget)
+        //{
+        //    this.transform.position += DirectionFromCameraToTarget / 100 * 0.1f;
+        //}
+        //else if (CurrentDictanceBetwenCameraAndTarget < MinDictanceBetwenCameraAndTarget)
+        //{
+        //    this.transform.position -= DirectionFromCameraToTarget / 100 * 0.1f;
+        //}
+
+        //LastFrameMouseY = m_MouseY;
 
     }
 
